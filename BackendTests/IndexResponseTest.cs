0eb75903ec986e3509714b623877a12df255970d
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-02-06 21:23:10 +0100

    Tests reference backend project

--- BackendTests/IndexResponseTest.cs
+++ BackendTests/IndexResponseTest.cs
@@ -1,4 +1,6 @@
+using System;
 using NUnit.Framework;
+using Backend;
 
 namespace BackendTests
 {
@@ -13,6 +15,22 @@ namespace BackendTests
         public void SanityCheck()
         {
             Assert.Pass();
+        }
+
+        [Test]
+        public void SetAndGet()
+        {
+            var today = DateTime.Today;
+            var message = "Testing, Jeremiah. How do you feel about birds taking the train?";
+
+            var response = new Backend.IndexResponse()
+            {
+                Date = today,
+                Message = message
+            };
+
+            Assert.AreEqual(today, response.Date);
+            Assert.AreEqual(message, response.Message);
         }
     }
 }
