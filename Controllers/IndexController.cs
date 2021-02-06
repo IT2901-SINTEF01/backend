4c17f6a005fdab7f4d46ae322213070f3f6a2693
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-02-06 20:14:34 +0100

    Intial commit
    Closes #1

---
+++ Controllers/IndexController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IndexController : ControllerBase
+    {
+        private readonly ILogger<IndexController> _logger;
+
+        public IndexController(ILogger<IndexController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IndexResponse Get()
+        {
+            return new IndexResponse()
+            {
+                Date = DateTime.Now.AddDays(1),
+                Message = "Hello, Index World!"
+            };
+        }
+    }
+}
---
+++ IndexResponse.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Backend
+{
+    public class IndexResponse
+    {
+        public DateTime Date { get; init; }
+
+        public string Message { get; init; }
+    }
+}
