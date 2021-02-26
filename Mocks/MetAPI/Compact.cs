badb1cdc2a942e26408ec650b3c3a77acd970765
Karl Petter Aubert <kpaubert@gmail.com>
2021-02-26 13:53:05 +0100

    Remove comments, r4m

--- Mocks/MetAPI/Compact.cs
+++ Mocks/MetAPI/Compact.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
 using Backend.Models.MetAPI.POCO;
-using BackendTests.utils;
+using Backend.utils;
 using Bogus;
 
 namespace Backend.Mocks.MetAPI
--- Models/SSB/AgesInNorway.cs
+++ Models/SSB/AgesInNorway.cs
@@ -3,19 +3,11 @@ using Backend.Models.JsonStat;
 
 namespace Backend.Models.SSB
 {
-    // Remove comments when merge-ready
-    // Based on https://data.ssb.no/api/v0/dataset/26975.json?lang=no
-    // https://data.ssb.no/api/v0/dataset/26975?lang=no
     public class AgesInNorway
     {
-        /*
-         * I tried abstracting this class as much as possible, so that the only thing
-         * that has to be changed when creating a new JSON-stat object, only the dimension
-         * needs to be changed.
-         */
-        [JsonPropertyName("dataset")] public JsonStatDataset<JsonStatSpecificDimension> Dataset { get; set; }
+        [JsonPropertyName("dataset")] public JsonStatDataset<AgesInNorwayDimension> Dataset { get; set; }
 
-        public sealed class JsonStatSpecificDimension : AbstractJsonStatDimension
+        public sealed class AgesInNorwayDimension : AbstractJsonStatDimension
         {
             [JsonPropertyName("Region")] public JsonStatDimensionContent Region { get; set; }
             [JsonPropertyName("Tid")] public JsonStatDimensionContent Tid { get; set; }
