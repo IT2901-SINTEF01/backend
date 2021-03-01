d404249b59898102b5f5178704ae6ff694b0409d
fredriwb <fredriwb@stud.ntnu.no>
2021-03-01 17:19:01 +0100

    Renaming and Sider fixes
    Yet to add  DP on the database

--- Models/Base/MetaData/POCO/Axis.cs
+++ Models/Base/MetaData/POCO/Axis.cs
@@ -1,14 +1,19 @@
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text.Json.Serialization;
 
 namespace Backend.Models.Base.MetaData.POCO
 {
-    public class Y
+    public class Axis
     {
+        public Axis(Collection<Limit> limit)
+        {
+            Limit = limit;
+        }
+
         [JsonPropertyName("name")] public string Name { get; set; }
 
         [JsonPropertyName("type")] public string Type { get; set; }
 
-        [JsonPropertyName("limit")] public List<Limit> Limit { get; set; }
+        [JsonPropertyName("limit")] public Collection<Limit> Limit { get; }
     }
 }
--- Models/Base/MetaData/POCO/Segment.cs
+++ Models/Base/MetaData/POCO/Segment.cs
@@ -1,12 +1,17 @@
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text.Json.Serialization;
 
 namespace Backend.Models.Base.MetaData.POCO
 {
     public class Segment
     {
+        public Segment(Collection<Value> value)
+        {
+            Value = value;
+        }
+
         [JsonPropertyName("name")] public string Name { get; set; }
 
-        [JsonPropertyName("value")] public List<Value> Value { get; set; }
+        [JsonPropertyName("value")] public Collection<Value> Value { get; }
     }
 }
