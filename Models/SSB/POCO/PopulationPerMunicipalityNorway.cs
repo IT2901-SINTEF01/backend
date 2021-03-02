2e0d006a7eac56828066d1814f7e6e5b4d9e7e75
Karl Petter Aubert <kpaubert@gmail.com>
2021-03-02 19:35:28 +0100

    Remove wrong class

--- Models/SSB/POCO/PopulationPerMunicipalityNorway.cs
+++ Models/SSB/POCO/PopulationPerMunicipalityNorway.cs
@@ -3,11 +3,11 @@ using Backend.Models.Base.JsonStat;
 
 namespace Backend.Models.SSB.POCO
 {
-    public class AgesInNorway
+    public class PopulationPerMunicipalityNorway
     {
-        [JsonPropertyName("dataset")] public JsonStatDataset<AgesInNorwayDimension> Dataset { get; set; }
+        [JsonPropertyName("dataset")] public JsonStatDataset<PopulationInNorwayDimension> Dataset { get; set; }
 
-        public sealed class AgesInNorwayDimension : AbstractJsonStatDimension
+        public sealed class PopulationInNorwayDimension : AbstractJsonStatDimension
         {
             [JsonPropertyName("Region")] public JsonStatDimensionContent Region { get; set; }
             [JsonPropertyName("Tid")] public JsonStatDimensionContent Tid { get; set; }
