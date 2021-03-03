e8d78aa7e280be9c094d45615dd688182b3e730c
Karl Petter Aubert <kpaubert@gmail.com>
2021-03-03 12:21:46 +0100

    Add skeleton

---
+++ BackendTests/UnitTests/PopulationInNorwayTests.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace BackendTests.UnitTests
+{
+    public class PopulationInNorwayTests
+    {
+        
+        
+        [SetUp]
+        public void Setup()
+        {
+            return;
+        }
+
+        [Test]
+        public void PopulationInNorwayAPIShouldReturnData()
+        {
+            return;
+        }
+    }
+}
