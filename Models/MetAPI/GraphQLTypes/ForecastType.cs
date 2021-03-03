c2c203d8d02c54c0d42cac4981992c22f3073bf2
fredriwb <fredriwb@stud.ntnu.no>
2021-03-03 13:33:34 +0100

    Metadata as nested query + mocking support

--- Models/MetAPI/GraphQLTypes/ForecastType.cs
+++ Models/MetAPI/GraphQLTypes/ForecastType.cs
@@ -1,20 +1,21 @@
-﻿using Backend.Models.MetAPI.POCO;
+﻿using Backend.API.Services;
+using Backend.Models.Base.Metadata.GraphQLTypes;
+using Backend.Models.MetAPI.POCO;
 using GraphQL.Types;
 
 namespace Backend.Models.MetAPI.GraphQLTypes
 {
     public sealed class ForecastType : ObjectGraphType<Forecast>
     {
-        public ForecastType()
+        public ForecastType(IMetadataService metadataService)
         {
             Field(forecast => forecast.ForecastGeometry, false, typeof(GeometryType))
                 .Description("The Geo-Data used in the query");
             Field(forecast => forecast.ForecastProperties, false, typeof(PropertiesType))
                 .Description("Data and meta-data returned");
             Field(forecast => forecast.Type);
-
-            // TODO: Implement metadata into the query
-            // Field(forecast => forecast.StoredMetadata, false, typeof(StoredMetadataType));
+            Field<StoredMetadataType>().ResolveAsync(context => metadataService.GetMetadata("MetAPI Forecast"))
+                .Name("metadata").Description("Metadata for the forecast data source");
         }
 
         /*
