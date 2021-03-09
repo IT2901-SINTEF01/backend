7d9fea472752d887d04296ea8d316198c8bfcf2f
fredriwb <fredriwb@stud.ntnu.no>
2021-03-09 17:02:57 +0100

    Add Metadata test

--- BackendTests/UnitTests/MetadataTest.cs
+++ BackendTests/UnitTests/MetadataTest.cs
@@ -1,7 +1,43 @@
+using System.Collections.ObjectModel;
+using Backend.Mocks.Metadata;
+using Backend.Models.Base.Metadata.POCO;
+using Xunit;
+
 namespace BackendTests.UnitTests
 {
     public class MetadataTest
     {
-        
+        private readonly StoredMetadata _storedMetadata;
+        private readonly Collection<StoredMetadata> _storedMetadatas;
+
+        public MetadataTest()
+        {
+            _storedMetadata = MockMetadata.GenerateStoredMetadata();
+            _storedMetadatas = MockMetadata.GenerateMultipleStoredMetadata();
+        }
+
+        [Fact]
+        public void MetadataShouldReturnData()
+        {
+            Assert.NotNull(_storedMetadata);
+        }
+
+        [Fact]
+        public void MetadatasShouldReturnData()
+        {
+            Assert.NotNull(_storedMetadatas);
+        }
+
+        [Fact]
+        public void MetaDataHasUrl()
+        {
+            Assert.Contains("http", _storedMetadata.Source);
+        }
+
+        [Fact]
+        public void MetadatasHasGivenLength()
+        {
+            Assert.Equal(10, _storedMetadatas.Count);
+        }
     }
 }
