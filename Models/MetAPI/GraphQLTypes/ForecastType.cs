31e2b0b223489fb0cac156272f30060c436da21c
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-03-17 12:36:40 +0100

    Use generic class for "injecting" metadata field into GraphQL type

--- Models/MetAPI/GraphQLTypes/ForecastType.cs
+++ Models/MetAPI/GraphQLTypes/ForecastType.cs
@@ -1,25 +1,20 @@
 ﻿using Backend.API.Services;
 using Backend.Models.Base.Metadata.GraphQLTypes;
 using Backend.Models.MetAPI.POCO;
+using Backend.utils.GraphQLTypes;
 using GraphQL.Types;
 
 namespace Backend.Models.MetAPI.GraphQLTypes
 {
-    public sealed class ForecastType : ObjectGraphType<Forecast>
+    public sealed class ForecastType : ObjectGraphTypeWithMetadata<Forecast>
     {
-        public ForecastType(IMetadataService metadataService)
+        public ForecastType(IMetadataService metadataService) : base(metadataService, "MetAPI Forecast")
         {
             Field(forecast => forecast.ForecastGeometry, false, typeof(GeometryType))
                 .Description("The Geo-Data used in the query");
             Field(forecast => forecast.ForecastProperties, false, typeof(PropertiesType))
                 .Description("Data and meta-data returned");
             Field(forecast => forecast.Type);
-            Field<StoredMetadataType>().ResolveAsync(async _ => await metadataService.GetMetadata("MetAPI Forecast"))
-                .Name("metadata").Description("Metadata for the forecast data source");
         }
-
-        /*
-         * The expression continues to resolve the type until the object is completely nested
-         */
     }
 }
---
+++ utils/GraphQLTypes/ObjectGraphTypeWithMetadata.cs
@@ -0,0 +1,21 @@
+using Backend.API.Services;
+using Backend.Models.Base.Metadata.GraphQLTypes;
+using GraphQL.Types;
+
+namespace Backend.utils.GraphQLTypes
+{
+    /// <summary>
+    /// Creates an object graph type (from GraphQL) with the associated metadata field retrieved from a MongoDB database.
+    /// </summary>
+    /// <typeparam name="T">The type which contains the data for which the metadata is associated.</typeparam>
+    public abstract class ObjectGraphTypeWithMetadata<T> : ObjectGraphType<T>
+    {
+        protected ObjectGraphTypeWithMetadata(IMetadataService metadataService, string metadataDocumentName)
+        {
+            Field<StoredMetadataType>()
+                .ResolveAsync(async _ => await metadataService.GetMetadata(metadataDocumentName))
+                .Name("metadata")
+                .Description($"Metadata for a data source with the name {metadataDocumentName}.");
+        }
+    }
+}
