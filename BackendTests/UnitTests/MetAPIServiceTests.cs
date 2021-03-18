6cd7af19e9f3633f93637a4518602b0d0db97258
fredriwb <fredriwb@stud.ntnu.no>
2021-03-18 17:08:40 +0100

    Testing of MetAPIService

--- BackendTests/UnitTests/MetAPIServiceTests.cs
+++ BackendTests/UnitTests/MetAPIServiceTests.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Net.Http;
 using Backend.API.Services;
+using Moq;
 using Xunit;
 
 namespace BackendTests.UnitTests
@@ -8,6 +10,7 @@ namespace BackendTests.UnitTests
     {
         private readonly float _lat;
         private readonly float _lon;
+        private readonly MetAPIService _metAPIService;
         private readonly MetAPIServiceMocked _metAPIServiceMocked;
         private readonly Random _random;
 
@@ -18,6 +21,8 @@ namespace BackendTests.UnitTests
             _random = new Random();
             _lon = (float) _random.NextDouble() * (63.433f - 63.42f) + 63.42f;
             _lat = (float) _random.NextDouble() * (10.4f - 10.38f) + 10.38f;
+            var httpClient = Mock.Of<HttpClient>();
+            _metAPIService = new MetAPIService(httpClient);
         }
 
         [Fact]
@@ -30,6 +35,18 @@ namespace BackendTests.UnitTests
         public void MetAPIServiceMockedIsCorrectType()
         {
             Assert.IsType<MetAPIServiceMocked>(_metAPIServiceMocked);
+        }
+
+        [Fact]
+        public void MetAPIServiceIsNotNull()
+        {
+            Assert.NotNull(_metAPIService.GetCompactForecast(_lat, _lon));
+        }
+
+        [Fact]
+        public void MetAPIServiceIsCorrectType()
+        {
+            Assert.IsType<MetAPIService>(_metAPIService);
         }
     }
 }
