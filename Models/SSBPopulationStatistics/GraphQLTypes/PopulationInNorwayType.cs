e4985a638502316659b1fc2343ac4213dd6e8145
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-03-24 10:13:56 +0100

    Code cleanup

--- Models/SSBPopulationStatistics/GraphQLTypes/PopulationInNorwayType.cs
+++ Models/SSBPopulationStatistics/GraphQLTypes/PopulationInNorwayType.cs
@@ -6,7 +6,8 @@ namespace Backend.Models.SSB.GraphQLTypes
 {
     public sealed class PopulationInNorwayType : ObjectGraphTypeWithMetadata<PopulationPerMunicipalityNorway>
     {
-        public PopulationInNorwayType(IMetadataService metadataService) : base(metadataService, "Befolkning. Kommuner, pr. 1.1., 1986 - siste år")
+        public PopulationInNorwayType(IMetadataService metadataService) : base(metadataService,
+            "Befolkning. Kommuner, pr. 1.1., 1986 - siste år")
         {
             Field(poco => poco.Dataset, false, typeof(PopulationInNorwayDatasetType))
                 .Description("Dataset containing population statistics for Norway.");
