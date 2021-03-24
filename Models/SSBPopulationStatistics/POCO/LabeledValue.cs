d5fd42f6c7fb40660049917179fa3e91f93141f9
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-03-24 13:37:31 +0100

    Collection and property get/init
    Maybe KP made Sider?

--- Models/SSBPopulationStatistics/POCO/LabeledValue.cs
+++ Models/SSBPopulationStatistics/POCO/LabeledValue.cs
@@ -1,12 +1,13 @@
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text.Json.Serialization;
 
 namespace Backend.Models.SSBPopulationStatistics.POCO
 {
     public class LabeledValue
     {
-        public string municipality;
+        [JsonPropertyName("municipality")] public string Municipality { get; init; }
 
         /// Format: [ [ year, population ] ]
-        public List<PopulationForYear> populationForYear;
+        [JsonPropertyName("populationForYear")] public Collection<PopulationForYear> PopulationForYear { get; init; }
     }
 }
