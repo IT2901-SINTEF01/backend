739bba247c278ef88cfe21ea9325ae984e5cc876
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-03-24 14:13:44 +0100

    Test PopulationInNorwayDataset

---
+++ BackendTests/UnitTests/PopulationInNorwayDatasetTypeTests.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Backend.Models.Base.JsonStat;
+using Backend.Models.SSBPopulationStatistics.GraphQLTypes;
+using Backend.Models.SSBPopulationStatistics.POCO;
+using GraphQL;
+using GraphQL.Execution;
+using Xunit;
+
+namespace BackendTests.UnitTests
+{
+    public class PopulationInNorwayDatasetTypeTests
+    {
+        [Fact]
+        public void PopulationInNorwayDatasetCorrectOutput()
+        {
+            var actor = new PopulationInNorwayDatasetType();
+
+            Assert.NotNull(actor.GetField("value"));
+
+            var expected = new LabeledValue()
+            {
+                Municipality = "Halden",
+                PopulationForYear = new Collection<PopulationForYear>()
+                {
+                    new()
+                    {
+                        Population = 100,
+                        Year = "1986"
+                    }
+                }
+            };
+
+            var actual = (List<LabeledValue>) actor.GetField("value").Resolver.Resolve(new ResolveFieldContext()
+            {
+                Parent = new ResolveFieldContext()
+                {
+                    Arguments = new Dictionary<string, ArgumentValue>()
+                    {
+                        {
+                            "municipalities",
+                            new ArgumentValue(new List<string>() {"K-3001"}, ArgumentSource.Variable)
+                        },
+                        {"years", new ArgumentValue(new List<string>() {"1986"}, ArgumentSource.Variable)}
+                    }
+                },
+                Source = new JsonStatDataset<PopulationPerMunicipalityNorway.PopulationInNorwayDimension>()
+                {
+                    Dimension = new PopulationPerMunicipalityNorway.PopulationInNorwayDimension()
+                    {
+                        Size = new Collection<int>() {359, 36, 1}
+                    },
+                    Value = new Collection<int>() {100}
+                }
+            });
+
+            Assert.Equal(expected, actual[0]);
+        }
+    }
+}
