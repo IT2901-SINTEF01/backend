40c05c0d8bb42a37009cf63fd04dc5c527c56d2c
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-03-29 18:25:09 +0200

    Update test to use DatasourceId

--- BackendTests/UnitTests/MetadataServiceTests.cs
+++ BackendTests/UnitTests/MetadataServiceTests.cs
@@ -10,25 +10,23 @@ namespace BackendTests.UnitTests
     public class MetadataServiceTests
     {
         private readonly MetadataServiceMocked _metadataServiceMocked;
-        private readonly string _randomString;
 
         public MetadataServiceTests()
         {
-            _randomString = new Faker().Name.FirstName();
             _metadataServiceMocked = new MetadataServiceMocked();
         }
 
         [Fact]
         public void MetadataServiceMockedIsCorrectType()
         {
-            Assert.IsType<Task<StoredMetadata>>(_metadataServiceMocked.GetMetadata(_randomString));
+            Assert.IsType<Task<StoredMetadata>>(_metadataServiceMocked.GetMetadata(DatasourceId.MET_API));
             Assert.IsType<Task<Collection<StoredMetadata>>>(_metadataServiceMocked.GetAllMetadata());
         }
 
         [Fact]
         public void MetadataServiceMockedIsNotNull()
         {
-            Assert.NotNull(_metadataServiceMocked.GetMetadata(_randomString));
+            Assert.NotNull(_metadataServiceMocked.GetMetadata(DatasourceId.SSB_POPULATION));
             Assert.NotNull(_metadataServiceMocked.GetAllMetadata());
         }
     }
