2d4dcc93a196699ad99f8223b87b55b929bd8c82
Fredrik Malmo <fredrik.malmo@icloud.com>
2021-04-07 14:48:56 +0200

    Mocked visualisation type should be valid type

--- BackendTests/GraphQL/Resolvers/Metadata/VisualisationTypeTest.cs
+++ BackendTests/GraphQL/Resolvers/Metadata/VisualisationTypeTest.cs
@@ -1,21 +1,34 @@
+using System.Linq;
+using Backend.Mocks.Metadata;
 using Backend.Models.Base.Metadata.GraphQLTypes;
+using GraphQL;
+using Shouldly;
 using Xunit;
 
 namespace BackendTests.GraphQL.Resolvers.Metadata
 {
     public class VisualisationTypeTest
     {
-        private readonly VisualisationType _visualisationType;
-
-        public VisualisationTypeTest()
+        private readonly VisualisationType _visualisationType = new();
+        
+        private object ResolveField(string fieldName)
         {
-            _visualisationType = new VisualisationType();
+            return _visualisationType.GetField(fieldName).Resolver.Resolve(new ResolveFieldContext
+            {
+                Source = MockMetadata.GenerateStoredMetadata().Generate().Visualisations.First()
+            });
         }
 
         [Fact]
         public void TestType()
         {
-            Assert.IsType<VisualisationType>(_visualisationType);
+            _visualisationType.ShouldBeOfType<VisualisationType>();
+        }
+
+        [Fact]
+        public void CorrectTypes()
+        {
+            ResolveField("Type").ShouldBeOneOf("linechart", "thresholdchart");           
         }
     }
 }
