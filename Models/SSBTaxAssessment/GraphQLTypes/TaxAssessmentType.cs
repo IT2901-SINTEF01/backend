2847bb20161e82c63bd9674a868bda441d7d2661
fredrikbw <fredriwb@stud.ntnu.no>
2021-04-29 13:55:49 +0200

    Add new query municipalitiesWithKeys
    #Fix 146

--- Models/SSBTaxAssessment/GraphQLTypes/TaxAssessmentType.cs
+++ Models/SSBTaxAssessment/GraphQLTypes/TaxAssessmentType.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
+using System.Linq;
 using Backend.API.Services;
 using Backend.Models.Base.Metadata.POCO;
 using Backend.Models.SSBTaxAssessment.POCO;
+using Backend.utils;
 using Backend.utils.GraphQLTypes;
 using GraphQL.Types;
 
@@ -19,6 +21,17 @@ namespace Backend.Models.SSBTaxAssessment.GraphQLTypes
                     Description = "Which years to get the population for.",
                     argument => { argument.DefaultValue = new List<string>(); })
                 .Description("Dataset containing population statistics for Norway.");
+            
+            Field<ListGraphType<ListGraphType<StringGraphType>>>("municipalitiesWithKeys",
+                "A nested list with the key for the municipality, followed by the human readable name.", null, _ =>
+                {
+                    var keys = NorwayTools.MunicipalityCodeToIndexTaxes.Keys;
+                    var values = NorwayTools.MunicipalityCodeToMunicipalityNameTaxes.Values;
+
+                    return keys.Zip(values)
+                        .Select<(string, string), List<string>>(el => new List<string> {el.Item1, el.Item2});
+                }
+            );
         }
     }
 }
